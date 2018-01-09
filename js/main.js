77a04581252a70ee0761737e85a6f97e0b02bba7
Jemy1234 <ahmdtawfiek@gmail.com>
2018-01-09 21:37:50 +0200

    E learning

--- js/main.js
+++ js/main.js
@@ -1,4 +1,48 @@
-$(function(){
-    'use strict';
-    
+// $(function(){
+//     'use strict';
+//     $('#btnPopover').popover();
+// });
+
+// $(function () {
+//   $('.example-popover').popover({
+//     container: 'body'
+//   });
+// });
+
+// $(document).ready(function(){
+//     $('[data-toggle="popover"]').popover(); 
+// });
+
+// $(function () {
+  // $('[data-toggle="popover"]').popover();
+  // $('.example-popover').popover({
+  //   container: 'body'
+  // });
+  // $('.popover-dismiss').popover({
+  // trigger: 'focus'
+  // });
+
+  // $('#rivers').popover({
+  //   placement : 'bottom',
+  //   html : true,
+  //   title : '<a href="#">Your Bag is Empty</a>',
+  //   content : ''
+
+  // });
+  
+// });
+$(document).ready(function(){
+    $('[data-toggle="popover"]').popover({
+      html:true
+    });   
+        $('[rel="popover"]').popover({
+        container: 'body',
+        html: true,
+        content: function () {
+            var clone = $($(this).data('popover-content')).clone(true).removeClass('hide');
+            return clone;
+        }
+    }).click(function(e) {
+        e.preventDefault();
+    });
 });
